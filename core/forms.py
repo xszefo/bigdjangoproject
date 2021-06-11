c2a4d91142b94008f151cb4f7d15b9777f9c25ad
Piotr Sumelka <piotr.sumelka@gmail.com>
2021-06-11 18:52:44 +0200

    crispy-forms

--- core/forms.py
+++ core/forms.py
@@ -6,4 +6,4 @@ User = get_user_model()
 class CustomUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'password1', 'password2']
